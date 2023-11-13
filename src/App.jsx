f258516099c2e309cbed86124a0bb817bda1b9cb
Karan <karanabrahamjoseph12@gmail.com>
2023-11-13 11:46:37 +0530

    currency converter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainBody from './components/MainBody'
+import Input from './components/Input'
+
+const App = () => {
+  return (
+    <div>
+        <MainBody/>
+    </div>
+  )
+}
+
+export default App
